9c5ef8d62b9232bdcd92df500e3d6ceda6207732
Masayuki Kawamura <kwmr5864@gmail.com>
2016-10-13 20:18:12 +0900

    エンティティとテーブル作成処理を分離

--- kawanado/app/migration.py
+++ kawanado/app/migration.py
@@ -1,24 +1,5 @@
-import datetime
-from peewee import *
-
-db = MySQLDatabase(
-        database='kawanado',
-        user='kawanado',
-        password='kawanado',
-        host='127.0.0.1',
-        port=3306,
-)
-
-
-class Search(Model):
-    """
-    検索結果。
-    """
-    keyword = CharField(index=True)
-    created_at = DateTimeField(default=datetime.datetime.now)
-
-    class Meta:
-        database = db
+from peewee import IntegrityError
+from entities import db, Search
 
 if __name__ == '__main__':
     try:
